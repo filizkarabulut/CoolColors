9d63fac79919f12d1bbf97b2194b9a27b3b5f2eb
Filiz Karabulut <fkarabulut2006gmail.com>
2023-08-12 10:31:49 +0300

    Initial Commit

---
+++ CoolColors/CoolColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolColorsApp.swift
+//  CoolColors
+//
+//  Created by Filiz Karabulut on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoolColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
